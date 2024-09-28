f4e6fb378a2675f5362e76c93531b4a613247137
Andrea Coi <andrea.coi@outlook.it>
2024-09-28 13:21:43 +0200

    implements a function to find the exact divisors of given number (dividend)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
 fn main() {
-    let dividers = total_dividers(10);
-    println!("{:?}", dividers);
+    // debug - verify the correctness of my function, step by step.
+    // For now, I’m doing this without implementing complex tests.
+    // let dividers = total_dividers(10);
+    // println!("{:?}", dividers);
 }
 
 
 // In this function, I extract ALL POSSIBLE DIVISORS, whether they are decimal or perfect divisors.
 // Of course, I start from the number by subtracting one and NATURALLY excluding 0.
-fn total_dividers(starting_number:u128) -> Vec<u128> {
+fn total_divisors(starting_number:u128) -> Vec<u128> {
 
     // initialize an empty vector to store the results of the countdown.
-    let mut dividers = Vec::new();
+    let mut divisors = Vec::new();
     
     // use a reverse for loop to iterate over the divisors.
     // For each of them, use push to insert it into the vector in each round.
     for x in (2..starting_number).rev() {
-        dividers.push(x);
+        divisors.push(x);
     }
-    dividers 
+    divisors 
+}
+
+// This function accepts two arguments: the dividend and a list of all divisors.
+// As output, it should return the list of exact divisors.
+// An exact divisor is calculated within this function using Rust's built-in modulo function.
+// So, IF dividend % divisor == 0, then the divisor is exact.
+// If the divisor is exact, I insert it into a list of exact divisors (vector).
+fn exact_dividers(dividend:u128, divisors:Vec<u128>) -> Vec<u128> {
+
+    // initialize an empty vector to store the results of the calculated exact divisors.
+    let mut exact_divisors = Vec::new();
+    // use a for cycle to iterate over divisors and test if a divisor is exact.
+    for x in divisors {
+        if dividend % x == 0 {
+            // if divisor is exact insert into vector "exact_divisors"
+            exact_divisors.push(x);
+        }
+    }
+    exact_divisors
 }
