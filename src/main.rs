46a45bf12d70313fc4bffae2d92d860f6eed01c9
Andrea Coi <andrea.coi@outlook.it>
2024-09-28 12:59:36 +0200

    Implemented function to calculate all possible divisors (including decimal and perfect) using a reverse loop.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let dividers = total_dividers(10);
+    println!("{:?}", dividers);
+}
+
+
+// In this function, I extract ALL POSSIBLE DIVISORS, whether they are decimal or perfect divisors.
+// Of course, I start from the number by subtracting one and NATURALLY excluding 0.
+fn total_dividers(starting_number:u128) -> Vec<u128> {
+
+    // initialize an empty vector to store the results of the countdown.
+    let mut dividers = Vec::new();
+    
+    // use a reverse for loop to iterate over the divisors.
+    // For each of them, use push to insert it into the vector in each round.
+    for x in (2..starting_number).rev() {
+        dividers.push(x);
+    }
+    dividers 
 }
